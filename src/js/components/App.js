ef45549b35173bf088e8c5ecf47e3b208e27144b
majsy <madeleineauvinen@gmail.com>
2018-10-15 15:49:09 -0400

    set up structure

---
+++ src/js/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from '../../logo.svg';
+import '../../scss/components/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Hello world.
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
