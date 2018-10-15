5e61392e20bb3e4d12afe8e1f5ba05efad57244f
majsy <madeleineauvinen@gmail.com>
2018-10-15 17:22:39 -0400

    add scss loaders

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../../logo.svg';
-import '../../scss/components/App.css';
+import '../../scss/components/App.scss';
 
 class App extends Component {
   render() {
