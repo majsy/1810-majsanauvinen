1b787f14f3f7be7eda2bc44b3be88c60b2f2e148
majsy <madeleineauvinen@gmail.com>
2018-10-25 19:24:46 -0400

    add lazy load img component wip style project detail page

--- src/js/components/home/ProjectItem.js
+++ src/js/components/home/ProjectItem.js
@@ -10,7 +10,7 @@ class ProjectItem extends Component {
     return (
       <NavLink className={`ProjectItem ${lastItem ? 'lastItem' : ''}`} to={`${ROUTE.WORK}${project.id}`}>
         <h2 className="display2 title" dangerouslySetInnerHTML={{__html: project.title}}></h2>
-        <h3 className="display3 subtitle">{project.subtitle}</h3>
+        <h3 className="display3 subtitle" dangerouslySetInnerHTML={{__html: project.subtitle}}></h3>
       </NavLink>
     );
   }
--- src/js/components/work/ProjectDetailSection.js
+++ src/js/components/work/ProjectDetailSection.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import '../../../scss/components/work/_ProjectDetailSection.scss';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 export default class ProjectDetailsSection extends Component {
+  getProjectDetailItems() {
+    const { project } = this.props;
+
+    return project.detailItems.map((item, index) => (
+      <li className="display5" key={`detail_item_${project.title}_${index}`}>{item}</li>
+    ));
+  }
+
   getProjectContent() {
     const { project } = this.props;
 
     return project.content.map((item, index) => (
       <div className={`content ${!item.img ? 'no-image' : ''} 
         ${!item.copy ? 'no-copy' : ''}`} 
-        key={`item_${project.title}_${index}`}>
+        key={`content_${project.title}_${index}`}>
         { item.img ? <div className="img-container">
-          <img className="img" src={item.img} />
+          <LazyLoadImage
+            className="img"
+            alt={''}
+            height={'1000px'}
+            src={item.img}
+            width={'100%'} />
         </div> : null }
 
         { item.copy ? <p className={`display5 copy`}>{item.copy}</p> : null }
@@ -24,7 +38,12 @@ export default class ProjectDetailsSection extends Component {
     return (
       <div className="ProjectDetailSection">
         <h2 className="display2 title" dangerouslySetInnerHTML={{__html: project.title}}></h2>
-        <h3 className="display3 subtitle">{project.subtitle}</h3>
+        <h3 className="display3 subtitle" dangerouslySetInnerHTML={{__html: project.subtitle}}></h3>
+
+        <ul className="detail-item-list">
+          { this.getProjectDetailItems() }
+        </ul>
+
         <div className="content-wrapper">
           { this.getProjectContent() }
         </div>
