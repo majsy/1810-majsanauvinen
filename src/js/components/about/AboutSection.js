c2be9b31bf1b60c6a258a17737a770b699aad4d3
majsy <madeleineauvinen@gmail.com>
2018-10-30 16:31:18 -0400

    add education list add experience list fix about page responsive layout

--- src/js/components/about/AboutSection.js
+++ src/js/components/about/AboutSection.js
@@ -2,19 +2,55 @@ import React, { Component } from 'react';
 import '../../../scss/components/about/_AboutSection.scss';
 
 class AboutSection extends Component {
+  getExperienceItems() {
+    const { data } = this.props;
+
+    return data.experience.list.map((item, index) => (
+      <li key={`experience_${index}`}>
+        <h4 className="display4 item">{item.role}, {item.company}, {item.period}</h4>
+      </li>
+    ))
+  }
+
+  getEducationItems() {
+    const { data } = this.props;
+
+    return data.education.list.map((item, index) => (
+      <li key={`education_${index}`}>
+        <h4 className="display4 item">{item.program}, {item.school}, {item.period}</h4>
+      </li>
+    ))
+  }
+
   render() {
     const { data } = this.props;
 
     return (
       <div className="AboutSection">
-        <div className="left-container">
-          <p className="display5 copy" dangerouslySetInnerHTML={{__html: data.copy.p1}}></p>
-          <p className="display5 copy" dangerouslySetInnerHTML={{__html: data.copy.p2}}></p>
-          <p className="display5 copy" dangerouslySetInnerHTML={{__html: data.copy.p3}}></p>
-          
+        <div className="upper-container">
+          <div className="left-container">
+            <p className="display5 copy" dangerouslySetInnerHTML={{__html: data.copy.p1}}></p>
+            <p className="display5 copy" dangerouslySetInnerHTML={{__html: data.copy.p2}}></p>
+            
+          </div>
+          <div className="right-container">
+            <div className="img-container"></div>
+          </div>
         </div>
-        <div className="right-container">
-          <div className="img-container"></div>
+
+        <div className="lower-container">
+          <div className="experience-container">
+            <h3 className="display3 title">{data.experience.title}</h3>
+            <ul>
+              { this.getExperienceItems()}
+            </ul>
+          </div>
+          <div className="education-container">
+            <h3 className="display3 title">{data.education.title}</h3>
+            <ul>
+              { this.getEducationItems()}
+            </ul>
+          </div>
         </div>
       </div>
     );
